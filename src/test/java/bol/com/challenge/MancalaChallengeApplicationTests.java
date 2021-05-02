beeaf2649415a1fa09fa821ce8ef9d9515b554bc
e.shakeri <QWEasd123>
2021-05-02 15:52:31 +0430

    create project

---
+++ src/test/java/bol/com/challenge/MancalaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package bol.com.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MancalaChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
